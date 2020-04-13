21ad425c5e936df65bb7ebb23223b0e161b227bb
Jason Twichell <jtwichell@core-techs.net>
2020-04-13 17:57:42 -0400

    Added in some code to display the new exercises

---
+++ Falcon_PortfolioDemo/js/SunsetHills.js
@@ -0,0 +1,16 @@
+﻿function SunsetHills(buildings) {
+    //Set tallest to the smallest possible number
+    let tallest = Number.MIN_VALUE;
+
+    //Here I want to build a new array that contains just the building that can see the sunset
+    let sunsetViews = new Array();
+
+    for (var index = 0; index < buildings.length; index++) {
+        if (buildings[index] > tallest) {
+            tallest = buildings[index];
+            sunsetViews.push(`The Building in position ${index} with height ${tallest} can see the sunset. <br>`);
+        }
+    }
+
+    return sunsetViews.join("");
+}
