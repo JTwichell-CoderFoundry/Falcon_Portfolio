7fbe8a6fc0b3ad9e9d3a7023f07da4b2ca4f735b
Jason Twichell <jtwichell@core-techs.net>
2020-04-14 12:09:36 -0400

    Added validation using the numOnly class

--- Falcon_PortfolioDemo/js/SunsetHills.js
+++ Falcon_PortfolioDemo/js/SunsetHills.js
@@ -1,4 +1,33 @@
-﻿function SunsetHills(buildings) {
+﻿$(function () {
+
+    $(".numOnly").on("keypress", function (e) {        
+        if (!(e.keyCode >= 48 && e.keyCode <= 57) || (e.keyCode >= 96 && e.keyCode <= 105)) {
+            return false;
+        };
+      
+    });
+
+    function SunsetHills(buildings) {
+
+    //var errorMsg = "";
+    //for (var index = 0; index < buildings.length; index++) {
+    //    if (isNaN(buildings[index])) {
+    //        var value = $(`#hillsInput${index + 1}`).val();     
+    //        errorMsg += `Your input in position ${index + 1}: ${value} is bad and needs to be fixed \n\r`;
+    //        $(`#hillsInput${index + 1}`).val("");
+    //    }
+    //}
+    //if (errorMsg !== "") {
+    //    alert(errorMsg);
+    //    //blank out the bad data for the user if i wanted...
+
+    //    return 
+    //}
+
+
+
+
+
     //Set tallest to the smallest possible number
     let tallest = Number.MIN_VALUE;
 
@@ -14,3 +43,11 @@
 
     return sunsetViews.join("");
 }
+
+})
+
+
+
+
+
+
