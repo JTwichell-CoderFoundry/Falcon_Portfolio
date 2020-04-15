7e837867bfe901ad098789753cea37c2ed397fdd
Jason Twichell <jtwichell@core-techs.net>
2020-04-15 17:18:24 -0400

    Update FizzBuzz.js

--- Falcon_PortfolioDemo/Scripts/FizzBuzz.js
+++ Falcon_PortfolioDemo/Scripts/FizzBuzz.js
@@ -1,11 +1,16 @@
 ﻿$("#btnFizzBuzz").on("click", function () {
 
     //Step 1: Get the data
-    let fizz = Number($("#fizzNum").val());
-    let buzz = Number($("#buzzNum").val());
-    
+    let fizz = $("#fizzNum").val();
+    let buzz = $("#buzzNum").val();
+
+    if (fizz.length < 1 || buzz.length < 1) {
+        alert("Something is amiss");
+        return;
+    }
+
     //Step 2:
-    var arrResult = DoFizzBuzz(fizz, buzz);
+    var arrResult = DoFizzBuzz(+fizz, +buzz);
 
     //Step 3: Output
     $("#output").html(arrResult.join(" | "));
